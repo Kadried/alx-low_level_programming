a7a4849f49233918276f37cba5aaa7acae4174ac
Kadried <kadriemmanuel2020@gmail.com>
2023-09-11 07:20:53 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * main - A program that prints a line with puts function
+ * Return: 0 (Pass)
+ */
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzles");
+return (0);
+}
