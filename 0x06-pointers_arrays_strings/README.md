34bab5d89b6c923b4005bd01ced5c8132c813b51
Kadried <kadriemmanuel2020@gmail.com>
2023-09-21 04:34:42 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+Creating mg 0x06-pointers_arrays_strings
+writing and computing my codes for reference purpose
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN._H
+#define MAIN_H
+
+/**
+ * my header file that had all of the prototyoes for the 0x06.C - more pointers
+ */
+int _putchar(char C);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+#endif
