8e18e7658b8d9e317b9a610ba8b261bec8d5ea2b
Kadried <kadriemmanuel2020@gmail.com>
2023-09-23 08:12:30 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	putchar('0');
+
+	putchar('\n' + 1);
+
+	putchar('1');
+
+	putchar('\n' + 1);
+
+	putchar('2');
+
+	putchar('\n' + 1);
+
+	putchar('3');
+
+	putchar('\n' + 1);
+
+	putchar('4');
+
+	putchar('\n' + 1);
+
+	putchar('5');
+
+	putchar('\n' + 1);
+
+	putchar('6');
+
+	putchar('\n' + 1);
+
+	putchar('7');
+
+	putchar('\n' + 1);
+
+	putchar('8');
+
+	putchar('\n' + 1);
+
+	putchar('9');
+
+	putchar('\n');
+	return (0);
+}
