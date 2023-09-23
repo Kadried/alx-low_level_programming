c59fb722b7068d010208cef92a7b6e1316f88661
Kadried <kadriemmanuel2020@gmail.com>
2023-09-23 08:25:51 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,44 +6,14 @@
  */
 int main(void)
 {
-	putchar('0');
+	int n;
 
-	putchar('\n' + 1);
+	for (n = 0; n <= 9; n++)
 
-	putchar('1');
-
-	putchar('\n' + 1);
-
-	putchar('2');
-
-	putchar('\n' + 1);
-
-	putchar('3');
-
-	putchar('\n' + 1);
-
-	putchar('4');
-
-	putchar('\n' + 1);
-
-	putchar('5');
-
-	putchar('\n' + 1);
-
-	putchar('6');
-
-	putchar('\n' + 1);
-
-	putchar('7');
-
-	putchar('\n' + 1);
-
-	putchar('8');
-
-	putchar('\n' + 1);
-
-	putchar('9');
-
+	{
+		putchar(n + 48);
+	}
 	putchar('\n');
+
 	return (0);
 }
